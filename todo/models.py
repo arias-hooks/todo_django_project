4b4687a67057d11f9e317ecd49df547d0235a0ec
asakoshiro <koshikoshi7@icloud.com>
2023-02-24 18:11:51 +0900

    add: todo

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+Choice = (("danger", "high"), ("warning", "normal"), ("info", "low"))
+
 
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices=Choice
+    )
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.title
